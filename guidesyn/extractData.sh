a1d1020c19f1d7f485bddfb276edcd1af88d7bf9
Pavol Bielik <pbielik@acm.org>
2020-02-23 20:01:32 +0100

    Update extractData.sh

--- guidesyn/extractData.sh
+++ guidesyn/extractData.sh
@@ -2,7 +2,10 @@
 # Script to extract data and models
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-tar -C $DIR/core -xvzf $DIR/core/saved_modelsPaper.tar.gz
+
+wget http://files.srl.inf.ethz.ch/data/guiding_synthesizers/models.tar.gz $DIR/code/models.tar.gz
+
+tar -C $DIR/core -xvzf $DIR/core/models.tar.gz
 tar -C $DIR/dataset/data/ -xvzf $DIR/dataset/data/ablation_dataset.tar.gz
 tar -C $DIR/dataset/data/ -xvzf $DIR/dataset/data/ds.tar.gz
 tar -C $DIR/dataset/data/ -xvzf $DIR/dataset/data/dsplus.tar.gz
